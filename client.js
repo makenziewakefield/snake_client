91baa0be7cceb207bebae8ce0e948078c720033e
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-16 15:45:11 -0600

    moved the connect function from play.js to client.js and added event handlers when the connection is made

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    conn.write("Name: MBW")
+  })
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
