9a7b5958fbefcb98abbf0e57d41d403430e36d1c
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-17 14:10:08 -0600

    moved hard coded variables to constants.js and imported them into client.js

--- client.js
+++ client.js
@@ -1,16 +1,16 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     conn.write("Name: MBW");
-    // conn.write("Move: up");
   })
 
   conn.on("data", (data) => {
